7548346ed47ddc89bdf4b60c1723110e591a6e18
Madhavi <madhavi.16@cse.mrt.ac.lk>
2021-01-29 11:09:44 +0530

    integration to webapp

--- Web API/app.py
+++ Web API/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from GSOMPrediction import get_predictions_from_gsom
+from GSOMPrediction import get_predictions_from_gsom, get_predictions_list_from_gsom
 from CNNPrediction import get_predictions_from_cnn
 
 app = Flask(__name__)
@@ -14,5 +14,16 @@ def distressPrediction():
     distress_status = get_predictions_from_gsom(predictions)
     return jsonify({'status': distress_status})
 
+@app.route("/distressProbability", methods = ['POST'])
+def distressPredictionProbability():
+    file = request.files['audio']
+    file.save('audio_recording.wav')
+    predictions = get_predictions_from_cnn('audio_recording.wav')
+    pred_list = get_predictions_list_from_gsom(predictions)
+    num_zeros = pred_list.count(0)
+    num_ones = pred_list.count(1)
+    return jsonify({'prediction': [ round(num_zeros/len(pred_list),2),round(num_ones/len(pred_list),2) ]})
+
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0',port=5001)
